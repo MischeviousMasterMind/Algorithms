0cf5529f3db3f4324552a63d29009e82b1766067
0KAY0 <0KAY04557@gmail.com>
2024-12-19 03:25:21 -0800

    :)

--- src/SortingAlgorithms.java
+++ src/SortingAlgorithms.java
@@ -11,7 +11,7 @@ public class SortingAlgorithms {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public static void bubbleSort(int[] data) 
+	public static int[] bubbleSort(int[] data) 
 	{
 		boolean swapped = true;
 		
@@ -29,9 +29,11 @@ public class SortingAlgorithms {
 				}
 			}		
 		}	
+		
+		return data;
 	}
 	
-	public static void insertionSort(int[] data)
+	public static int[] insertionSort(int[] data)
 	{
 		for(int i = 0; i < data.length; i++)
 		{		
@@ -49,9 +51,11 @@ public class SortingAlgorithms {
 			data[i] 	= data[index];
 			data[index] = temp;	
 		}
+		
+		return data;
 	}
 	
-	public static void selectionSort(int[] data) 
+	public static int[] selectionSort(int[] data) 
 	{
 		for(int i = 1; i < data.length; i++)
 		{
@@ -65,9 +69,74 @@ public class SortingAlgorithms {
 				}
 			}
 		}
+		
+		return data;
 	}
 	
-	public static int[] generateRandomIntArray(int n) {
+	public static int[] mergeSort(int[] elements)
+    {
+        int n = elements.length;
+        int[] temp = new int[n];
+        mergeSortHelper(elements, 0, n - 1, temp);
+        
+        return elements;
+    }
+
+    private static void mergeSortHelper(
+        int[] elements, int from, int to, int[] temp)
+    {
+        if (from < to)
+        {
+            int middle = (from + to) / 2;
+            mergeSortHelper(elements, from, middle, temp);
+            mergeSortHelper(elements, middle + 1, to, temp);
+            merge(elements, from, middle, to, temp);
+        }
+    }
+
+    private static void merge(
+        int[] elements, int from, int mid, int to, int[] temp)
+    {
+        int i = from;
+        int j = mid + 1;
+        int k = from;
+
+        while (i <= mid && j <= to)
+        {
+            if (elements[i] < elements[j])
+            {
+                temp[k] = elements[i];
+                i++;
+            }
+            else
+            {
+                temp[k] = elements[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i <= mid)
+        {
+            temp[k] = elements[i];
+            i++;
+            k++;
+        }
+
+        while (j <= to)
+        {
+            temp[k] = elements[j];
+            j++;
+            k++;
+        }
+
+        for (k = from; k <= to; k++)
+        {
+            elements[k] = temp[k];
+        }
+    }
+	
+    public static int[] generateRandomIntArray(int n) {
 		
 		int [] data = new int[n];
 		
@@ -79,16 +148,15 @@ public class SortingAlgorithms {
 		}
 		
 		return data;
-		
 	}
 	
 	public static void main(String[] args)
 	{
 
-		int n		= 1000;
+		int n		= 9999;
 		int[] data	= generateRandomIntArray(n);
 		
-		System.out.println(Arrays.toString(data));
+//		System.out.println(Arrays.toString(data));
 		System.out.println();
 		
 		long start = System.nanoTime();
@@ -112,19 +180,56 @@ public class SortingAlgorithms {
 		System.out.println("Sorting time: " + (System.nanoTime() - start) / 1000 + " miliseconds");
 		System.out.println();
 		
-		System.out.println(Arrays.toString(data));
+		start = System.nanoTime();
+		
+		mergeSort(data);
+		System.out.println("MERGE SORT");
+		System.out.println("Sorting time: " + (System.nanoTime() - start) / 1000 + " miliseconds");
+		System.out.println();
+		
+//		System.out.println(Arrays.toString(data));
 		
 		try
 		{
 			PrintWriter output = new PrintWriter(myFile);
 			
-			output.println();
+			output.println("Number of Elements,Bubble Sort Time (ms),Selection Sort Time (ms),Insertion Sort Time (ms),Merge Sort Time (ms)");
+			System.out.println("Number of Elements,Bubble Sort Time (ms),Selection Sort Time (ms),Insertion Sort Time (ms),Merge Sort Time (ms)");
 			
-			for(int i = 1; i < 10; i++) {
+			for(int i = 0; i <= 10000; i += 100) {
 				
 				int[] list = generateRandomIntArray(i);
+				int bubbleSortTime = 0, insertionSortTime = 0, selectionSortTime = 0, mergeSortTime = 0;
+				
+				start = System.nanoTime();
+				bubbleSort(list);
+				bubbleSortTime = (int)(System.nanoTime() - start) / 1000;
+				
+				int[] sortedList = list;
+				
+				start = System.nanoTime();
+				insertionSort(list);
+				insertionSortTime = (int)(System.nanoTime() - start) / 1000;
+				
+				start = System.nanoTime();
+				selectionSort(list);
+				selectionSortTime = (int)(System.nanoTime() - start) / 1000;
+				
+				if(!sortedList.equals(list)) {
+					System.out.println(":(");
+					break;
+				}
+				
+				start = System.nanoTime();
+				mergeSort(list);
+				mergeSortTime = (int)(System.nanoTime() - start) / 1000;
+				
+				output.println(i + "," + bubbleSortTime + "," + insertionSortTime + "," + selectionSortTime + "," + mergeSortTime);
+				System.out.println(i + "," + bubbleSortTime + "," + insertionSortTime + "," + selectionSortTime + "," + mergeSortTime);
 				
 			}
+			
+			output.close();
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
